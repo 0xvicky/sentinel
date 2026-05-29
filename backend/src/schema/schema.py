45bf893d985694dc1021f548faeab761755c38bf
whyvickyyy <tyagivivek528@gmail.com>
2026-05-29 20:09:18 +0530

    feat(chunking):added chunking strategy

--- backend/src/schema/schema.py
+++ backend/src/schema/schema.py
@@ -8,3 +8,10 @@ class FileModel(BaseModel):
     file_size: int
     file_name: str
     session_id: str
+
+
+class ChunkModel(BaseModel):
+    chunk_id:int
+    content:str
+    doc_id:str
+    doc_path:str
--- backend/src/services/process_docs.py
+++ backend/src/services/process_docs.py
@@ -1,28 +1,82 @@
 from src.repository import get_doc
 from celery.utils.log import get_task_logger
+import fitz
 import pymupdf
+from src.schema.schema import ChunkModel, FileModel
+from typing import List
 
 logger = get_task_logger(__name__)
 
+# separator = ["\n\n", "\n", ".", ""]
 
-def parse_doc(doc_path: str):
-    doc = pymupdf.open(doc_path)
+
+def chunker(cln_txt: str, doc_metadata: FileModel):
+    CHUNK_SIZE = 500
+    OVERLAP = 50
+    STRIDE = CHUNK_SIZE - OVERLAP
+    i = 0
+    chunks: List[ChunkModel] = []
+    idx = 0
+
+    while i < len(cln_txt):
+        chunk_content = (
+            cln_txt[i:]
+            if len(cln_txt) - i < CHUNK_SIZE
+            else cln_txt[i : i + CHUNK_SIZE]
+        )
+        chunk = ChunkModel(
+            chunk_id=idx,
+            content=chunk_content,
+            doc_id=doc_metadata.file_id,
+            doc_path=doc_metadata.file_path,
+        )
+        chunks.append(chunk)
+        i += STRIDE
+        idx += 1
+
+    return chunks
+
+
+def extract(doc_path: str):
+    """Extract the content from the whole document"""
+    cleaned_data = ""
+
+    with fitz.open(doc_path) as doc:
+        for page_num in range(len(doc)):
+            # load_page
+            page = doc.load_page(page_num)
+            # clean the text
+            blocks = page.get_text("blocks")
+            for block in blocks:
+                text = block[4]
+                cleaned_data += text
+            # chunk the text based on certain strategy
+
+    return cleaned_data
+
+
+def parse_doc(doc_metadata: FileModel):
+    """Return chunks from the document for ingestion."""
+    # extract the cleaned data from doc
+    doc_path = doc_metadata.file_path
+    logger.info("Here it is")
+    clean_data = extract(doc_path)
+    # print(clean_data)
+    # recursive chunking
+    ch = chunker(clean_data, doc_metadata)
+
+    return ch
 
 
 def process(doc_id: str):
+    """Process each doc for vectorDB ingestion."""
     doc_metadata = get_doc(doc_id)
     # access that file first
-    doc_path = doc_metadata.file_path
     # logger.info(f"Here in process service:{doc_path}")
     # parse the document using pymupdf
-    chunks = parse_doc(doc_path)
+    chunks = parse_doc(doc_metadata)
     # log to see what you're getting
-    for i, chunk in enumerate(chunks):
-        logger.info(f"Chunk {i} | Section: {chunk['metadata']['section']}")
-        logger.info(f"Text preview: {chunk['text'][:100]}")
-
-    logger.info(f"Total chunks: {len(chunks)}")
-    
+    logger.info(chunks)
     # ingest it to vector db && bm25 indexing in postgres
 
     # if success, update the status to ready for query
